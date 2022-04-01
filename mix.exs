1da049cf4dfbbe185bd0a7724133ca8561621729
Mario Cassiano <mariocassiano@gmail.com>
2022-04-01 16:35:14 -0300

    chore: add excoveralls

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Appgit.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
